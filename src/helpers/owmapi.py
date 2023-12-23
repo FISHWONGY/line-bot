9f318e5e01977fa11e8eaa9d5037de4f8004ae93
FISHWONGY <59711659+FISHWONGY@users.noreply.github.com>
2023-12-23 22:37:32 +0000

    Update owmapi.py

--- src/helpers/owmapi.py
+++ src/helpers/owmapi.py
@@ -1,25 +1,21 @@
 import re
+from typing import Tuple
 from helpers.utils import mgr, owm_temp_metrics, owm_rep_dict, owm_status_replace_dict
 
 
 def clean_data(str_obj: str, replace_dict: dict) -> str:
     rep = dict((re.escape(k), v) for k, v in replace_dict.items())
     pattern = re.compile("|".join(rep.keys()))
-    str_obj = pattern.sub(lambda m: rep[re.escape(m.group(0))], str_obj)
+    clean_str = pattern.sub(lambda m: rep[re.escape(m.group(0))], str_obj)
 
-    return str_obj
+    return clean_str
 
 
-def get_weather_deatils(location: str):
-    obs = mgr.weather_at_place(f"{location}")
-    w = obs.weather
-    temp = w.temperature("celsius")
-    for e in owm_temp_metrics:
-        temp.pop(e)
-    temp2 = str(temp)
-    temp2 = clean_data(temp2, owm_rep_dict)
+def get_weather_details(location: str) -> Tuple[str, str]:
+    obs = mgr.weather_at_place(location)
+    temp = {key: value for key, value in obs.weather.temperature("celsius").items() if key not in owm_temp_metrics}
+    temp_str = clean_data(str(temp), owm_rep_dict)
 
-    status = w.detailed_status
-    status = clean_data(status, owm_status_replace_dict)
+    status = clean_data(obs.weather.detailed_status, owm_status_replace_dict)
 
-    return temp2, status
+    return temp_str, status
