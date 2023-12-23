7e4cdfcdfccaaff3adc39f7de4b177c59efa9271
FISHWONGY <59711659+FISHWONGY@users.noreply.github.com>
2023-12-23 21:50:01 +0000

    Update owmapi.py

--- src/helpers/owmapi.py
+++ src/helpers/owmapi.py
@@ -2,6 +2,14 @@ import re
 from helpers.utils import mgr, owm_temp_metrics, owm_rep_dict, owm_status_replace_dict
 
 
+def clean_data(str_obj: str, replace_dict: dict) -> str:
+    rep = dict((re.escape(k), v) for k, v in replace_dict.items())
+    pattern = re.compile("|".join(rep.keys()))
+    str_obj = pattern.sub(lambda m: rep[re.escape(m.group(0))], str_obj)
+
+    return str_obj
+
+
 def get_weather_deatils(location: str):
     obs = mgr.weather_at_place(f"{location}")
     w = obs.weather
@@ -9,14 +17,9 @@ def get_weather_deatils(location: str):
     for e in owm_temp_metrics:
         temp.pop(e)
     temp2 = str(temp)
-
-    rep = dict((re.escape(k), v) for k, v in owm_rep_dict.items())
-    pattern = re.compile("|".join(rep.keys()))
-    temp2 = pattern.sub(lambda m: rep[re.escape(m.group(0))], temp2)
+    temp2 = clean_data(temp2, owm_rep_dict)
 
     status = w.detailed_status
-    status_replace = dict((re.escape(k), v) for k, v in owm_status_replace_dict.items())
-    status_pattern = re.compile("|".join(status_replace.keys()))
-    status = status_pattern.sub(lambda m: status_replace[re.escape(m.group(0))], status)
+    status = clean_data(status, owm_status_replace_dict)
 
     return temp2, status
